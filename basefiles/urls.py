6e5603946a6b5c1e4f052637a23cac4a9f630b55
Daniel Abercrombie <dabercro@mit.edu>
2016-06-16 13:51:24 +0200

    URLs.

--- basefiles/urls.py
+++ basefiles/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'^$', 'showlog.showlog.give_logs', name='Unified Logs'),
+    url(r'^showlog/', 'showlog.showlog.give_logs', name='Unified Logs'),
+    url(r'^4dinfo/', '4dinfo.4dinfo.return_page', name='Errors'),
 )
